78da372329a8368c0a16e64f53aa420aacde2c91
Ben Homan <benjayhoman@gmail.com>
2026-09-16 14:55:57 -0400

    Cleaned up program model

---
+++ internal/model/program/focus.go
@@ -0,0 +1,46 @@
+package program
+
+import (
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/pigeon_carrier/internal/app"
+)
+
+// returns all focusable elements in the program so that the current focus can be managed
+func (m *Program) focusables() []app.Focusable {
+	focusables := make([]app.Focusable, 0, 3) // initial capacity for method, urlInput, and add header
+	focusables = append(focusables, m.method, m.urlInput)
+
+	for _, focusable := range m.headers.GetFocusables() {
+		focusables = append(focusables, focusable)
+	}
+	return focusables
+}
+
+// calls the Focus method for what currently has focus and Blurs all others
+func (m *Program) setFocus() (tea.Model, tea.Cmd) {
+	return m.incrementAndSetFocus(0)
+}
+
+// increments the current focus by the given amount and updates the focus state of all focusable elements
+func (m *Program) incrementAndSetFocus(increment int) (tea.Model, tea.Cmd) {
+	focusables := m.focusables()
+	length := len(focusables)
+
+	if increment >= 0 {
+		m.currentFocus = (m.currentFocus + increment) % length
+
+	} else {
+		m.currentFocus = (m.currentFocus + increment + length) % length
+	}
+
+	var cmd []tea.Cmd
+	for i, f := range focusables {
+		if i == m.currentFocus {
+			cmd = append(cmd, f.Focus())
+
+		} else {
+			cmd = append(cmd, f.Blur())
+		}
+	}
+	return m, tea.Batch(cmd...)
+}
--- internal/model/program/program.go
+++ internal/model/program/program.go
@@ -3,7 +3,6 @@ package program
 import (
 	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
-	"github.com/pigeon_carrier/internal/app"
 	"github.com/pigeon_carrier/internal/model/headers"
 	"github.com/pigeon_carrier/internal/model/method"
 	"github.com/pigeon_carrier/internal/model/urlinput"
@@ -26,50 +25,10 @@ func NewProgram() Program {
 		urlInput:     urlInput,
 		headers:      headers,
 	}
-	program.setFocus()
-
+	program.setFocus() // set initial focus to the first focusable element
 	return program
 }
 
 func (m Program) Init() tea.Cmd {
 	return textinput.Blink
-}
-
-func (m *Program) focusables() []app.Focusable {
-	focusables := make([]app.Focusable, 0)
-	focusables = append(focusables, m.method, m.urlInput)
-
-	for _, focusable := range m.headers.GetFocusables() {
-		focusables = append(focusables, focusable)
-	}
-	return focusables
-}
-
-func (m *Program) setFocus() (tea.Model, tea.Cmd) {
-	return m.incrementAndSetFocus(0)
-}
-
-func (m *Program) incrementAndSetFocus(increment int) (tea.Model, tea.Cmd) {
-	focusables := m.focusables()
-	length := len(focusables)
-
-	if increment >= 0 {
-		m.currentFocus = (m.currentFocus + increment) % length
-
-	} else {
-		m.currentFocus = (m.currentFocus + increment + length) % length
-	}
-
-	var cmd []tea.Cmd
-
-	for i, f := range focusables {
-		if i == m.currentFocus {
-			cmd = append(cmd, f.Focus())
-
-		} else {
-			cmd = append(cmd, f.Blur())
-		}
-	}
-
-	return m, tea.Batch(cmd...)
 }
